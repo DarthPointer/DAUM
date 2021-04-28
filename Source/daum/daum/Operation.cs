7d6c016d1bbe537c03221411cdb739e5b4be75b5
DarthPointer <al.kiriackin@yandex.ru>
2021-04-28 22:12:40 +0300

    Basement and name add feature

---
+++ Source/daum/daum/Operation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using DRGOffSetterLib;
+
+namespace daum
+{
+    public abstract class Operation
+    {
+        public abstract string ExecuteAndGetOffSetterAgrs(ref Span<byte> span, List<string> args);
+
+        protected static Span<byte> Insert(Span<byte> span, Span<byte> insert, Int32 offset)
+        {
+            Span<byte> spanStart = span.Slice(0, offset);
+            Span<byte> spanEnd = span.Slice(offset);
+
+            byte[] newArray = new byte[span.Length + insert.Length];
+            spanStart.ToArray().CopyTo(newArray, 0);
+            insert.ToArray().CopyTo(newArray, offset);
+            spanEnd.ToArray().CopyTo(newArray, offset + insert.Length);
+
+            return newArray;
+        }
+
+        protected static Span<byte> StringToBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                bytes[i] = (byte)str[i];
+            }
+
+            return bytes;
+        }
+    }
+
+    public abstract class MapOperation : Operation
+    {
+        private static string addOpKey = "-a";
+        private static string replaceKey = "-r";
+        private static string byIndexKey = "-i";
+
+        protected abstract Int32 nextBlockOffsetOffset { get; }
+
+        public override string ExecuteAndGetOffSetterAgrs(ref Span<byte> span, List<string> args)
+        {
+            string opKey = args.TakeArg();
+            if (opKey == addOpKey)
+            {
+                return " -n" + AddOperation(ref span, args, DOLib.Int32FromSpanOffset(span, nextBlockOffsetOffset));
+            }
+
+            return "";
+        }
+
+        protected abstract string AddOperation(ref Span<byte> span, List<string> args, Int32 offset);
+    }
+
+    public class NameDefOperation : MapOperation
+    {
+        protected override Int32 nextBlockOffsetOffset => 69;
+
+        protected override string AddOperation(ref Span<byte> span, List<string> args, int offset)
+        {
+            Span<byte> insert = new Span<byte>(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+            insert = Insert(insert, StringToBytes(args[0]), 4);
+
+            DOLib.WriteInt32IntoOffset(insert, args[0].Length + 1, 0);
+            span = Insert(span, insert, offset);
+
+            return $" {insert.Length} {1}";
+        }
+    }
+}
---
+++ Source/daum/daum/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.IO;
+using DRGOffSetterLib;
+using System.Reflection;
+
+namespace daum
+{
+    class Program
+    {
+        private static string configPath;
+
+        private static string exitCommand = "exit";
+
+        private static Dictionary<string, Operation> operations = new Dictionary<string, Operation>() {
+            { "-n", new NameDefOperation() }
+        };
+
+        static void Main(string[] args)
+        {
+            configPath = Assembly.GetExecutingAssembly().Location;
+            configPath = configPath.Substring(0, configPath.LastIndexOf('\\') + 1) + "Config.json";
+
+            //Console.WriteLine(configPath);
+            Config config = GetConfig();
+            RunData runData = ProcessArgs(args);
+
+            //Console.WriteLine(config.offsetterPath);
+
+            if (runData.fileName.Length > 0)
+            {
+                bool runLoop = true;
+
+                Span<byte> span = File.ReadAllBytes(runData.fileName);
+
+                Console.WriteLine($"Drg Automation Utility for Modding welcomes you!");
+                Console.WriteLine($"Entered interactive mode for file {runData.fileName}");
+                Console.WriteLine();
+
+                while (runLoop)
+                {
+                    try
+                    {
+                        runLoop = ProcessCommand(ref span, config, runData, out string offSetterCallArgs);
+
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception!");
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+            }
+            
+            //WriteConfig(config);
+        }
+
+        private static Config GetConfig()
+        {
+            Config config;
+            if (File.Exists(configPath))
+            {
+                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configPath), new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+            }
+            else
+            {
+                config = new Config();
+            }
+
+            return config;
+        }
+
+        private static void WriteConfig(Config config)
+        {
+            File.WriteAllText(configPath, JsonConvert.SerializeObject(config, new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            }));
+        }
+
+        private static RunData ProcessArgs(string[] args)
+        {
+            RunData runData = new RunData();
+
+            if (args.Length > 0)
+            {
+                runData.fileName = args[0];
+            }
+
+            return runData;
+        }
+
+        private static bool ProcessCommand(ref Span<byte> span, Config config, RunData runData, out string offSetterCallArgs)
+        {
+            List<string> command = new List<string>(Console.ReadLine().Split(' '));
+
+            if (command.Count > 0)
+            {
+                if (command[0].Length > 0)
+                {
+                    if (command[0] == exitCommand)
+                    {
+                        offSetterCallArgs = "";
+                        return false;
+                    }
+
+                    string operationKey = command.TakeArg();
+
+                    if (operations.ContainsKey(operationKey))
+                    {
+                        offSetterCallArgs = operations[operationKey].ExecuteAndGetOffSetterAgrs(ref span, command);
+
+                        if (File.Exists(runData.fileName + ".daum")) File.Delete(runData.fileName + ".daum");
+                        Directory.Move(runData.fileName, runData.fileName + ".daum");
+                        File.WriteAllBytes(runData.fileName, span.ToArray());
+
+                        Process offSetter = Process.Start(config.offsetterPath, runData.fileName + offSetterCallArgs + " -m -r");
+
+                        offSetter.WaitForExit();
+                        span = File.ReadAllBytes(runData.fileName);
+
+                        Console.WriteLine("Done!");
+                        return true;
+                    }
+                }
+            }
+
+            offSetterCallArgs = "";
+            return true;
+        }
+
+        private record RunData
+        {
+            public string fileName = "";
+        }
+
+        [JsonObject]
+        private class Config
+        {
+            public string offsetterPath = "";
+        }
+    }
+}
